64295668d6cc38aa293407b9e249ac98d3e40796
Amanda Aziz <142847924+Amanda-Aziz@users.noreply.github.com>
2026-08-31 15:39:38 -0300

    Update pilha.c

--- estrutura-de-dados/PILHA/pilha.c
+++ estrutura-de-dados/PILHA/pilha.c
@@ -1,2 +1,44 @@
-#include <stdio.h>
-printf("oi");
+#include<stdlib.h>
+#include"pilha.h"
+
+struct pilha{ // stack
+    int dados[MAX];
+    int topo;
+};
+
+Pilha criar(){
+    Pilha p = malloc(sizeof(struct pilha));
+    if(p != NULL){
+        p->topo = 0;
+    }
+    return p;
+}
+
+// push
+int empilhar(Pilha p, int valor){
+    if(p->topo < MAX){
+        p->dados[p->topo] = valor;
+        p->topo++;
+        return 1;
+    }
+    return 0;
+} 
+//peek
+int acessar_topo(Pilha p){
+    if(p->topo == 0) // piha vazia
+        return 0;
+    return p->dados[p->topo - 1];
+}
+// pop
+int desempilhar(Pilha p){
+    if(p->topo == 0) // piha vazia
+        return 0;
+    p->topo--;
+    return 1;
+} 
+
+// free
+void destruir(Pilha p) {
+    if(p != NULL)
+        free(p);
+}
