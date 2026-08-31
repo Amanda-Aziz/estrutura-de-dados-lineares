091a61c97788a49d4988a131982873d0c86370f7
Amanda Aziz <142847924+Amanda-Aziz@users.noreply.github.com>
2026-08-31 15:38:55 -0300

    Update quadrado.c

--- estrutura-de-dados/TAD/QUADRADO/quadrado.c
+++ estrutura-de-dados/TAD/QUADRADO/quadrado.c
@@ -1,1 +1,46 @@
+#include<stdlib.h>
+#include"quadrado.h"
 
+struct quadrado{
+    float lado;
+    float area;
+    float perimetro;
+};
+
+Quadrado* criar(float lado){
+    Quadrado* q = malloc(sizeof(Quadrado));
+    if(q != NULL){
+        if(lado <= 0)
+            lado = 1;    
+        q->lado = lado;
+        q->area = q->lado*q->lado;
+        q->perimetro = 4*q->lado;    
+    }
+    return q;
+}
+
+float acessar(Quadrado* q, char var){
+    if(var == 'L')
+        return q->lado;
+    if(var == 'A')
+        return q->area;
+    if(var == 'P') 
+        return q->perimetro;
+    return -1;
+}
+
+int alterar(Quadrado* q, float lado){
+    if(lado <= 0)
+        return -1;
+    else{
+        q->lado = lado;
+        q->area = q->lado*q->lado;
+        q->perimetro = 4*q->lado;    
+        return 1;
+    }
+}
+
+void destruir(Quadrado* q){
+    if(q != NULL)
+        free(q);
+}
