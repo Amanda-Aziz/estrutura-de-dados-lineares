bcb35bb968d79c68960d6367919a8b34ef95d0a8
m4nd1skx <amandaaziz103@gmail.com>
2026-09-22 20:25:37 -0300

    lista encadeada ok

--- estrutura-de-dados/LISTA-ENCADEADA/lista_int.c
+++ estrutura-de-dados/LISTA-ENCADEADA/lista_int.c
@@ -2,7 +2,7 @@
 
 struct elem{
     int valor;
-    struct elem* prox;
+    Elem* prox;
 };
 typedef struct elem Elem;
 
@@ -12,7 +12,7 @@ struct lista{
 };
 typedef struct lista* Lista;
 
-// =================================================== MÉTODOS
+// metodos
 
 Lista criar_lista(Lista li){
     Lista li = malloc(sizeof(struct lista));
@@ -56,27 +56,88 @@ int inserir_final(Lista li, int valor_inserir){
     return 0;
 }
 
-int remover_inicio(Lista li){ //acessar lista, acessar inicio, remover inicio
-    if(li->qtd == 0){
-        return 0;
-    }
+int remover_inicio(Lista li){
+    if(li->qtd == 0) return 0;
     Elem* aux = li->inicio;
     li->inicio = aux->prox;
     free(aux);
     li->qtd--;
+    return 1;
 }
 
 int acessar_inicio(Lista li){
-    if(li->qtd == 0){
-        return 0;
-    }
+    if(li->qtd == 0) return 0;
     return li->inicio->valor;
 }
 
-void remover_final(){}
+void destruir(Lista li){
+    if(li == NULL) return;
+    Elem* aux = li->inicio;
+    while(aux->prox != NULL){
+        Elem* atual = aux;
+        aux = aux->prox;
+        free(atual);
+    }
+    free(aux);
+    free(li);
+}
 
-void destruir(Lista li){}
-void imprimir_lista(Lista li){}
-int acessar_final(){}
-void buscar_por_valor(){}
-void buscar_por_posicao(){}
+int remover_final(Lista li){
+    if(li->qtd == 0) return 0;
+    Elem* aux = li->inicio;
+    if(aux->prox == NULL){
+        free(aux);
+        li->inicio = NULL;
+        li->qtd--;
+        return 1;
+    }
+    Elem* ant;
+    while(aux->prox != NULL){
+        ant = aux;
+        aux = aux->prox;
+    }
+    free(aux);
+    ant->prox = NULL;
+    li->qtd--;
+    return 1;
+}
+
+int acessar_final(Lista li){
+    if(li->qtd == 0){
+        return 0;
+    }
+    Elem* aux = li->inicio;
+    
+    while(aux->prox != NULL){
+        aux = aux->prox;
+    }
+    return aux->valor;
+}
+
+int buscar_por_valor(Lista li, int valor){
+    Elem* aux = li->inicio;
+    int posicao = 0;
+
+    while(aux != NULL){
+         if(aux->valor == valor){
+            return posicao;
+         }
+         aux = aux->prox;
+         posicao++;
+    }
+
+    return 0;
+}
+
+int buscar_por_posicao(Lista li, int posicao){
+    if(posicao < 0 || posicao >= li->qtd){
+        return 0;
+    }
+    Elem* aux = li->inicio;
+
+    for(int i=0; i < posicao; i++){
+        aux = aux->prox;
+    }
+
+    return aux->valor;
+}
